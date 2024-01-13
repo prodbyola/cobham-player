6372b95549bcd6644a4b548b2f394c0d3371d4b8
Ajayi Olamide Daniel <prodbyola@gmail.com>
2024-01-13 13:06:33 +0100

    Adding center.

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <div class="cs-player-page">
+      <div class="cs-player">
+        <Sticker />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import Sticker from './components/TapeSticker.vue';
+</script>
+
+<style lang="scss">
+@import url('https://fonts.cdnfonts.com/css/sf-pro-display');
+@import url('https://fonts.cdnfonts.com/css/sf-compact-display');
+@import url('https://fonts.cdnfonts.com/css/arial');
+@import 'font/ds/stylesheet.css';
+
+.cs-player-page {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(239deg, #607CDE 27.11%, #38BF8E 73.91%);
+
+  .cs-player {
+    width: 542px;
+    height: 334px;
+    background-color: #030303;
+    padding: 42px 32px;
+  }
+}
+</style>
---
+++ src/components/TapeSticker.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="csp__sticker">
+        <div class="csp__sticker_redtape"></div>
+        <div class="csp__sticker_whitetape">
+            <div class="csp__sticker_sidea">
+                <p class="sidea__text">A</p>
+            </div>
+            <p class="csp__sticker_acst_text">acoustic cassette</p>
+            <p class="csp__scripted">muffana</p>
+            <p class="csp__sticker_text">N.R.[ ] IN OUT</p>
+        </div>
+        <div class="csp__sticker_redtape"></div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.csp__sticker {
+    .csp__sticker_redtape {
+        height: 12px;
+        background-color: #DD2828;
+    }
+
+    .csp__sticker_whitetape {
+        p {
+            color: #100F0F;
+            font: SF Pro Display;
+        }
+
+        display: flex;
+        width: 100%;
+        height: 48px;
+        background-color: white;
+        padding: 9px 18px;
+
+        .csp__sticker_sidea {
+            height: 24px;
+            width: 24px;
+            background-color: #100F0F;
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+
+            .sidea__text {
+                font-family: Arial, Helvetica, sans-serif;
+                color: white;
+                font-weight: 700;
+                font-size: 1.33331rem;
+            }
+        }
+
+        .csp__sticker_acst_text {
+            text-transform: uppercase;
+            font-size: 0.625rem;
+            font-weight: 500;
+            font-family: SF Pro Display;
+            margin-left: 4px;
+        }
+
+        .csp__scripted {
+            font-family: Disruptors Script;
+            font-weight: 400;
+            font-size: 4rem;
+            line-height: 0.4;
+            transform: rotate(-4.577deg);
+            margin-left: 12px;
+            margin-right: 12.4px;
+        }
+
+        .csp__sticker_text {
+            font-size: 0.625rem;
+        }
+    }
+}
+</style>
