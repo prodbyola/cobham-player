62540e5c574a9289a2bd038bd2388ee96a4dfc83
Ajayi Olamide Daniel <prodbyola@gmail.com>
2024-01-15 08:20:39 +0100

    Added tape holes.

--- src/components/cassette/CassetteIndex.vue
+++ src/components/cassette/CassetteIndex.vue
@@ -4,6 +4,7 @@
       <TapeCover />
       <Sticker />
       <TapeInfo />
+      <TapeSubHoles />
     </div>
 </template>
 <script setup lang="ts">
@@ -11,6 +12,7 @@ import TapeReels from './TapeReels.vue';
 import TapeCover from './TapeCover.vue';
 import Sticker from './TapeSticker.vue';
 import TapeInfo from './TapeInfo.vue';
+import TapeSubHoles from './TapeSubHoles.vue';
 </script>
 
 <style lang="scss" scoped>
@@ -19,7 +21,7 @@ import TapeInfo from './TapeInfo.vue';
     min-width: 542px;
     min-height: 334px;
     border: solid 1px #030303;
-    padding: 42px 32px;
+    padding: 42px 32px 0px 32px;
     border-radius: 20px;
     filter: drop-shadow(-5px 5px 6px rgba(29, 80, 65, 0.50));
   }
---
+++ src/components/cassette/TapeSubHoles.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="csp_subholes">
+    <div class="csp_hole__container">
+        <div class="csp_hole csp_hole__rounded csp_page_bg"></div>
+        <div class="csp_hole csp_hole__square csp_page_bg"></div>
+    </div>
+
+    <div class="csp_hole__container container_left">
+        <div class="csp_hole csp_hole__rounded csp_page_bg"></div>
+        <div class="csp_hole csp_hole__square csp_page_bg"></div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+$rounded_size: 24px;
+$square_size: 20px;
+.csp_subholes {
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 48px;
+  justify-content: center;
+  column-gap: 136px;
+
+  .csp_hole {
+    background-attachment: fixed;
+    fill: transparent;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
+
+  .csp_hole__container {
+    display: inline-flex;
+    column-gap: 32px;
+      .csp_hole__rounded {
+        height: $rounded_size;
+        width: $rounded_size;
+        border-radius: 50%;
+        margin-top: 20px;
+      }
+
+      .csp_hole__square {
+        height: $square_size;
+        width: $square_size;
+        border-radius: 5px;
+        margin-top: 16px;
+      }
+  }
+
+  .csp_hole__container.container_left {
+    flex-direction: row-reverse;
+  }
+
+}
+</style>
