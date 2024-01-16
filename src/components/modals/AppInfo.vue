837784abe19819f2a20edba6611282516dc569b9
Ajayi Olamide Daniel <prodbyola@gmail.com>
2024-01-16 14:00:50 +0100

    Added favicon.

--- src/components/modals/AppInfo.vue
+++ src/components/modals/AppInfo.vue
@@ -1,6 +1,7 @@
 <template>
   <AppModal title="App Info">
     <div class="csp_info">
+      <h4 class="open_text">Time to Back in Time &#128337; &#128515;!</h4>
       <p>
         <span class="csp_strong">Cobhams Player</span> is a retro-style cassette player created just
         for the fun of it, dedicated to the reowned Performing Artist, Singer-Songwriter and Music
@@ -23,6 +24,9 @@
         <a href="https://www.figma.com/@tank666" target="_blank">Yury Zeliankousk</a> for the
         original <a href="https://www.figma.com/community/file/935937278123360532">Figma template</a>.
       </p>
+      <p>
+        Favicon: <a href="https://www.freepik.com/free-vector/cinema-film-production-realistic-transparent-composition-with-isolated-image-circle-bobbin-with-reel-vector-illustration_23130020.htm#query=reel&position=3&from_view=search&track=sph&uuid=ec86461c-d4dd-4322-a77d-606cf3ae3a53">Image by macrovector</a> on Freepik
+      </p>
     </div>
   </AppModal>
 </template>
@@ -32,6 +36,13 @@ import AppModal from './AppModal.vue'
 <style lang="scss" scoped>
 .csp_info {
   margin: 18px 8px;
+
+  .open_text {
+    text-align: center;
+    font-size: 1.4rem;
+    color: rgba(0, 0, 0, 0.627);
+    font-weight: bold;
+  }
   a,
   .csp_strong {
     font-weight: bold;
