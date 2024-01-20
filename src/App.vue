aa51ab5e65af201154a39e722e9f07b304f64233
edvardFH <97302653+edvardFH@users.noreply.github.com>
2024-01-20 22:55:32 +0100

    Player page height now adapts to header's height to avoid scroll bar from appearing

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,7 @@ body {
 .cs-player-page {
   overflow-x: hidden;
   width: 100vw;
-  height: 100vh;
+  height: calc(100vh - var(--header-height));
   display: flex;
   justify-content: center;
   align-items: center;
